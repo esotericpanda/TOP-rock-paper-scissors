b27bff9068ceb865c1578c9cc84117760d150478
jurada <tokontoszkolne@gmail.com>
2022-08-23 15:45:10 +0200

    finish game

--- rps.js
+++ rps.js
@@ -1,28 +1,38 @@
 
-function getComputerChoice(){
-    switch(Math.floor(Math.random()*3)){
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
         case 0:
-            return "Rock"
+            return "Rock";
         case 1:
-            return "Paper"
+            return "Paper";
         case 2:
-            return "Scissors"
+            return "Scissors";
     }
 }
 
 
 
-function playRound(playerSelection,computerSelection){
-    playerSelectionLower=playerSelection.toLowerCase();
-    computerSelectionLower=computerSelection.toLowerCase;
+function playRound(playerSelection, computerSelection) {
+    playerSelectionLower = playerSelection.toLowerCase();
+    computerSelectionLower = computerSelection.toLowerCase();
 
-    if(playerSelectionLower==computerSelection) return "It's a Tie!";
+    if (playerSelectionLower === computerSelectionLower) return "It's a Tie!";
 
-    if(playerSelectionLower==="rock"&&computerSelectionLower==="scissors" || playerSelectionLower==="scissors"&&computerSelectionLower==="paper"|| playerSelectionLower==="paper"&&computerSelectionLower==="rock"){
+    if (playerSelectionLower === "rock" && computerSelectionLower === "scissors" || playerSelectionLower === "scissors" && computerSelectionLower === "paper" || playerSelectionLower === "paper" && computerSelectionLower === "rock") {
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
 
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
-
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
 
 }
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        const playerSelection=prompt('Choose "rock", "paper" or "scissors"');
+        const computerSelection=getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        
+    }
+}
+game();
